d4003906c22d44114f9a6c4c2e729345f3a1b035
TwinFan <twinfan@sonux.net>
2022-01-29 21:18:04 +0100

    Added MSVC toolchain file, reorganized some build files

--- docker/build-win.cmd
+++ docker/build-win.cmd
@@ -18,11 +18,16 @@ MKDIR "%MY_BUILD_DIR%""
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 PUSHD "%MY_BUILD_DIR%"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+REM Output some environment information for analysis purposes
+ECHO MY_VC_BASE = %MY_VC_BASE%
+ECHO MY_BUILD_DIR = %MY_BUILD_DIR%
 ECHO I am now in: %CD%
+SET
 
 REM Create Makefile from CMakeLists.txt, using NMake output beacause NMake happens to be available
 REM CMAKE -G "NMake Makefiles" -DCMAKE_BUILD_TYPE:STRING="RelWithDebInfo" -DCMAKE_INSTALL_PREFIX:PATH="%CD%\install\x64-Release" -DCMAKE_CXX_FLAGS:STRING="/DWIN32 /D_WINDOWS /DWIN32_LEAN_AND_MEAN /D_CRT_SECURE_NO_WARNINGS /GS- /Gd /Gy- /Zc:wchar_t /Zc:forScope /Zc:inline /Zc:__cplusplus /W4 /EHsc /fp:precise /diagnostics:caret" -DCMAKE_CXX_FLAGS_RELWITHDEBINFO:STRING="/MD /Zi /O2 /DNDEBUG" -DCMAKE_C_COMPILER:FILEPATH="cl.exe" -DCMAKE_CXX_COMPILER:FILEPATH="cl.exe"  -DCMAKE_MAKE_PROGRAM="nmake.exe" ..
-CMAKE -G "NMake Makefiles" -DCMAKE_BUILD_TYPE:STRING="RelWithDebInfo" -DCMAKE_MAKE_PROGRAM="nmake.exe" -DCMAKE_TOOLCHAIN_FILE="..\docker\Toolchain-vs-x86-64.cmake" ..
+CMAKE -G "NMake Makefiles" -DCMAKE_BUILD_TYPE:STRING="RelWithDebInfo" -DCMAKE_MAKE_PROGRAM="nmake.exe" -DCMAKE_TOOLCHAIN_FILE="..\docker\Toolchain-msvc-x86-64.cmake" ..
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 REM Perform the actual build
